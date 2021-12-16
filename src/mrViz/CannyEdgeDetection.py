0353cd0a46437d03f73a2e5fbd0eba37cca4f910
Simon Rask Langkilde <simon.rask.langkilde@gmail.com>
2021-12-16 10:35:45 +0100

    commit for save

--- src/mrViz/CannyEdgeDetection.py
+++ src/mrViz/CannyEdgeDetection.py
@@ -1,167 +1,182 @@
-import math
+from typing import Tuple
 
-import matplotlib.pyplot as plt
+import numba as nb
 import numpy as np
+from numba import njit, prange
+from numba.core.types.containers import UniTuple
+from numba.core.types.npytypes import Array
+
+# Set numba image type
+ImageType = Array(nb.float64, 2, "C")
 
 
-class cannyEdge:
+@njit((ImageType)(nb.int64, nb.float64))
+def GaussFilter(kernel_size: int = 3, sigma: float = 1.4) -> np.ndarray:
+    """Gaussian Filter
+
+    Method that creates Gaussian filter
+
+    Parameters
+    ----------
+    kernel_size : int, optional
+        The kernel size, must be odd, by default 3
+    sigma : float, optional
+        The sigma value, determines the blur, by default 1.4
+
+    Returns
+    -------
+    np.ndarray
+        The gaussian filter, a kernel_size x kernel_size matrix.
     """
-    Written by Simon Rask
-    Source: https://en.wikipedia.org/wiki/Canny_edge_detector
 
-    * Apply Gaussian filter to smooth the image in order to remove the noise
-    * Find the intensity gradients of the image
-        * We use the Sobel operator
-        * Source: https://en.wikipedia.org/wiki/Sobel_operator
-    * Apply non-maximum suppression
-        We apply a non-maximum suppression to get rid of spurious
-        response to edge detection
-    * Apply double threshold to determine potential edges
-    * Track edge by hysteresis:
-        Finalize the detection of edges by suppressing all the other
-        edges that are weakand not connected to strong edges.
+    # Initialize kernel
+    kernel = np.zeros((kernel_size, kernel_size), dtype=np.float64)
+
+    k = (kernel_size - 1) // 2
+    for i in range(1, kernel_size + 1):
+        for j in range(1, kernel_size + 1):
+            top = (i - (k + 1)) ** 2 + (j - (k + 1)) ** 2
+            bott = 2 * sigma ** 2
+            kernel[i - 1, j - 1] = np.exp(-top / bott)
+    kernel *= 1 / (2 * np.pi * sigma ** 2)
+
+    return kernel
+
+
+@njit((ImageType)(ImageType, ImageType), parallel=True)
+def conv(img: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    """Image convolution
+
+    Preforms the convolution:
+        output = img * kernel, where * denotes convolution
+    and
+        output[i, j] = sum_m^n kernel[m,n]*img[i+m,j+n]
+
+    Parameters
+    ----------
+    img : np.ndarray
+        An image
+    kernel : np.ndarray
+        convolution kernel
+
+    Returns
+    -------
+    np.ndarray
+        convoluded image
     """
 
-    def __init__(self):
-        """
-        TODO: Consider if there are some stuff that are nice to initialize
-        """
-        return
+    # Dimensions of the original image
+    h_img, w_img = img.shape
 
-    def GaussFilter(
-        self, kernel_size: int = 3, sigma: float = 1.4
-    ) -> np.ndarray:
-        """Gaussian Filter
+    # Set kernel size
+    kernel_size, _ = kernel.shape
+    k = (kernel_size - 1) // 2
 
-        Method that creates Gaussian filter
+    # Zero-padding the image
+    img_padded = np.zeros((h_img + kernel_size - 1, w_img + kernel_size - 1))
+    img_padded[k:-k, k:-k] = img
 
-        Parameters
-        ----------
-        kernel_size : int, optional
-            The kernel size, must be odd, by default 3
-        sigma : float, optional
-            The sigma value, determines the blur, by default 1.4
+    # Initialize output image
+    out_img = np.zeros_like(img)
 
-        Returns
-        -------
-        np.ndarray
-            The gaussian filter, a kernel_size x kernel_size matrix.
-        """
-        # Error Checking - kernel dimensions must be odd
-        if kernel_size % 2 == 0:
-            return -2
+    # Image convolution
+    # here using numba.prange in order to execute in parallel
+    for row in prange(h_img):
+        for col in prange(w_img):
+            for i in prange(kernel_size):
+                for j in prange(kernel_size):
+                    out_img[row, col] += (
+                        img_padded[row + i, col + j] * kernel[i, j]
+                    )
+    return out_img
 
-        # Initialize kernel
-        kernel = np.zeros((kernel_size, kernel_size))
 
-        k = (kernel_size - 1) // 2
-        for i in range(kernel_size):
-            for j in range(kernel_size):
-                top = (i - (k + 1)) ** 2 + (j - (k + 1)) ** 2
-                bott = 2 * sigma ** 2
-                kernel[i, j] = np.exp(-top / bott)
-        kernel *= 1 / (2 * math.pi * sigma ** 2)
+@njit((UniTuple(ImageType, 2))(ImageType))
+def Sobel_operator(img: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """Sobel Operator
 
-        return kernel
+    Applies the Sobel operator to an image.
 
-    def Sobel_operator(
-        self, img: np.ndarray, show: bool = False
-    ) -> np.ndarray:
-        """[summary]
+    Parameters
+    ----------
+    img : np.ndarray
+        The image which to apply the Sobel operator
 
-        Parameters
-        ----------
-        img : np.ndarray
-            The image which to apply the Sobel operator
-        show : bool, optional
-            if True, the result of the operator is displayed, by default Fasle
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        The Gradient magnitude of the image, and the Gradient direction
+    """
 
-        Returns
-        -------
-        np.ndarray
-            The sobel operator applied to the image.
-        """
-        kernel_x = np.array(
-            [
-                [-1, 0, 1],
-                [-2, 0, 2],
-                [-1, 0, 1],
-            ]
-        )
+    kernel_x = np.array(
+        [
+            [-1, 0, 1],
+            [-2, 0, 2],
+            [-1, 0, 1],
+        ],
+        dtype=np.float64,
+    )
 
-        kernel_y = np.array(
-            [
-                [-1, -2, -1],
-                [0, 0, 0],
-                [1, 2, 1],
-            ]
-        )
-        G_x = self.conv(img, kernel_x)
+    kernel_y = np.array(
+        [
+            [-1, -2, -1],
+            [0, 0, 0],
+            [1, 2, 1],
+        ],
+        dtype=np.float64,
+    )
+    G_x = conv(img, kernel_x)
+    G_y = conv(img, kernel_y)
+    G_return = np.zeros(img.shape)
 
-        # If show is True then show the image
-        if show:
-            plt.imshow(G_x, cmap="gray")
-            plt.title("Horizontal Edge")
-            plt.show()
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            G_return[i, j] = (G_x[i, j] ** 2 + G_y[i, j] ** 2) ** (1 / 2)
 
-        G_y = self.conv(img, kernel_y)
-        # If show is True then show the image
-        if show:
-            plt.imshow(G_y, cmap="gray")
-            plt.title("vertical Edge")
-            plt.show()
+    # G_return = G_return / G_return.max() * 255
+    theta = np.arctan2(G_y, G_x)
 
-        G_return = np.zeros(img.shape)
+    return G_return, theta
 
-        for i in range(img.shape[0]):
-            for j in range(img.shape[1]):
-                G_return[i, j] = (G_x[i, j] ** 2 + G_y[i, j] ** 2) ** (1 / 2)
 
-        return G_return
+@njit((ImageType)(ImageType), parallel=True)
+def reflect_angle(T):
+    N, M = T.shape
+    for i in prange(N):
+        for j in prange(M):
+            if T[i, j] < 0:
+                T[i, j] += np.pi
+    return T
 
-    def conv(self, img, kernel, Extend=False):
-        """
-        Function that convoludes the image as follows
-        img[i,j] = sum_m^n b[m,n]*img[i+m,j+n]
-        """
-        # Error Checking - inputs
-        if not all(
-            [isinstance(img, np.ndarray), isinstance(kernel, np.ndarray)]
-        ):
-            return -1
 
-        # Dimensions of the original image
-        h_img = img.shape[0]
-        w_img = img.shape[1]
+@njit((ImageType)(ImageType, ImageType))
+def non_max_suppression(G, theta):
+    # Reflect angles
+    theta = reflect_angle(theta)
 
-        # Error Checking - kernel must be square
-        if kernel.shape[0] != kernel.shape[1]:
-            return -2
+    M, N = G.shape
+    Z = np.zeros((M, N), dtype=np.float64)
 
-        # Set kernel size
-        kernel_size = kernel.shape[0]
-        k = (kernel_size - 1) // 2
+    for i in range(1, M - 1):
+        for j in range(1, N - 1):
+            g0 = 255
+            g1 = 255
+            angle = theta[i, j]
+            if (0 <= angle < np.pi / 8) or (7 * np.pi / 8 <= angle):
+                g0 = G[i, j + 1]
+                g1 = G[i, j - 1]
+            elif np.pi / 8 <= angle < 3 * np.pi / 8:
+                g0 = G[i + 1, j - 1]
+                g1 = G[i - 1, j + 1]
+            elif 3 * np.pi / 8 <= angle < 5 * np.pi / 8:
+                g0 = G[i + 1, j]
+                g1 = G[i - 1, j]
+            elif 5 * np.pi / 8 <= angle < 7 * np.pi / 8:
+                g0 = G[i - 1, j - 1]
+                g1 = G[i + 1, j + 1]
 
-        # Error Checking - kernel dimensions must be odd
-        if kernel_size % 2 == 0:
-            return -2
+            if (G[i, j] >= g0) and (G[i, j] >= g1):
+                Z[i, j] = G[i, j]
 
-        # Zero-padding the image
-        img_padded = np.zeros(
-            (h_img + kernel_size - 1, w_img + kernel_size - 1)
-        )
-        img_padded[k:-k, k:-k] = img
-
-        # Initialize output image
-        out_img = np.zeros_like(img)
-
-        # Image convolution
-        for row in range(h_img):
-            for col in range(w_img):
-                for i in range(kernel_size):
-                    for j in range(kernel_size):
-                        out_img[row, col] += (
-                            img_padded[row + i - k, col + j - k] * kernel[i, j]
-                        )  # TODO: GO FIX!
-
-        return out_img
+    # Z = Z / Z.max() * 255
+    return Z
